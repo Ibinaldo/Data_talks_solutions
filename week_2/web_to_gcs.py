a83a6c6597740235fae6d26128ba768364176b97
Ibinabo Bestmann <ibi.best@gmail.com>
2023-02-06 13:50:25 -0800

    parameterized function

--- week_2/web_to_gcs.py
+++ week_2/web_to_gcs.py
@@ -37,11 +37,11 @@ def write_gcs(path: Path) -> None:
 
 
 @flow()
-def web_to_gcs() -> None:
+def web_to_gcs(year: int = 2020, color: str = "green", month: int = 11) -> None:
     '''The main ETL function'''
-    color = "green"
-    year = 2020
-    month = 11
+    # color = "green"
+    # year = 2020
+    # month = 11
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url= f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
